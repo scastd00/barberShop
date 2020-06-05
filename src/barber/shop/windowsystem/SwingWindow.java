57f52e01d37b5112f94d29b149b776195058f849
scastd00 <scastd00@estudiantes.unileon.es>
2020-06-06 01:21:36 +0200

    Tabla añadida

--- src/barber/shop/windowsystem/SwingWindow.java
+++ src/barber/shop/windowsystem/SwingWindow.java
@@ -4,6 +4,8 @@ import barber.shop.*;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.LineBorder;
 
 public class SwingWindow {
 
@@ -13,6 +15,7 @@ public class SwingWindow {
 	private JTextField txtMinute;
 	private BarberShop bs;
 	private GUI gui;
+	private JTable table;
 
 	/**
 	 * Create the application.
@@ -45,7 +48,7 @@ public class SwingWindow {
 
 		JComboBox<String> comboBox = new JComboBox<>();
 		comboBox.setModel(new DefaultComboBoxModel<>(comboBoxList));
-		comboBox.setBounds(284, 175, 158, 24);
+		comboBox.setBounds(235, 107, 181, 24);
 		frmBarberShop.getContentPane().add(comboBox);
 
 		// Buttons start //
@@ -54,7 +57,7 @@ public class SwingWindow {
 		 * Add reservation button
 		 */
 		JButton btnAddReservation = new JButton("Add Reservation");
-		btnAddReservation.setBounds(62, 234, 131, 25);
+		btnAddReservation.setBounds(23, 169, 150, 25);
 		btnAddReservation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent add) {
 				String place = comboBoxList[comboBox.getSelectedIndex()];
@@ -67,7 +70,7 @@ public class SwingWindow {
 		 * Cancel reservation button
 		 */
 		JButton btnCancelReservation = new JButton("Cancel Reservation");
-		btnCancelReservation.setBounds(526, 234, 170, 25);
+		btnCancelReservation.setBounds(443, 169, 170, 25);
 		btnCancelReservation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent cancel) {
 				String place = comboBoxList[comboBox.getSelectedIndex()];
@@ -80,7 +83,7 @@ public class SwingWindow {
 		 * Modify reservation button
 		 */
 		JButton btnModifyReservation = new JButton("Modify Reservation");
-		btnModifyReservation.setBounds(272, 234, 170, 25);
+		btnModifyReservation.setBounds(225, 169, 170, 25);
 		btnModifyReservation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent modify) {
 				String place = comboBoxList[comboBox.getSelectedIndex()];
@@ -97,21 +100,50 @@ public class SwingWindow {
 		frmBarberShop.getContentPane().setLayout(null);
 		txtCompleteName = new JTextField();
 		txtCompleteName.setText("Complete Name");
-		txtCompleteName.setBounds(284, 33, 114, 19);
+		txtCompleteName.setBounds(235, 22, 181, 19);
 		frmBarberShop.getContentPane().add(txtCompleteName);
 		txtCompleteName.setColumns(10);
 
 		txtHour = new JTextField();
 		txtHour.setText("Hour");
-		txtHour.setBounds(284, 74, 114, 19);
+		txtHour.setBounds(235, 63, 51, 19);
 		frmBarberShop.getContentPane().add(txtHour);
 		txtHour.setColumns(10);
 
 		txtMinute = new JTextField();
 		txtMinute.setText("Minute");
-		txtMinute.setBounds(284, 125, 114, 19);
+		txtMinute.setBounds(298, 63, 57, 19);
 		frmBarberShop.getContentPane().add(txtMinute);
 		txtMinute.setColumns(10);
+		
+		table = new JTable();
+		table.setFillsViewportHeight(true);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setSurrendersFocusOnKeystroke(true);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				"Name", "Time", "Place"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(250);
+		table.getColumnModel().getColumn(0).setMinWidth(250);
+		table.getColumnModel().getColumn(1).setPreferredWidth(50);
+		table.getColumnModel().getColumn(2).setPreferredWidth(125);
+		table.getColumnModel().getColumn(2).setMinWidth(125);
+		table.setBorder(new LineBorder(Color.DARK_GRAY));
+		table.setBounds(650, 65, 427, 144);
+		frmBarberShop.getContentPane().add(table);
 		
 		// THIS MUST BE HERE
 		frmBarberShop.setVisible(true);
