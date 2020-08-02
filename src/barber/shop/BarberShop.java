c57c1fd12367e7543adce7b64fedf2fd3b2c2397
scastd00 <scastd00@estudiantes.unileon.es>
2020-08-02 13:48:11 +0200

    Añadida librería JSON

--- src/barber/shop/BarberShop.java
+++ src/barber/shop/BarberShop.java
@@ -3,6 +3,7 @@ package barber.shop;
 import barber.shop.exceptions.BarberException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.*;
 
 /**
  * Class that makes all the transactions of a barber shop.
---
+++ src/barber/shop/JsonTests.java
@@ -0,0 +1,10 @@
+package barber.shop;
+
+import org.json.*;
+
+public class JsonTests {
+	public void hola() {
+		JSONObject obj = new JSONObject();
+		obj.getJSONArray("1").get(1);
+	}
+}
