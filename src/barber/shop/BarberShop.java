aa599a761c59dd27f0123572567d61366ec07d6d
scastd00 <scastd00@estudiantes.unileon.es>
2020-08-07 00:05:42 +0200

    Eliminado json de BarberShop

--- src/barber/shop/BarberShop.java
+++ src/barber/shop/BarberShop.java
@@ -3,7 +3,6 @@ package barber.shop;
 import barber.shop.exceptions.BarberException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.*;
 
 /**
  * Class that makes all the transactions of a barber shop.
